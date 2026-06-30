eccc63fb370daa1f89e1271e6d746c56c4a4fbb5
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2026-06-30 13:16:59 +0530

    Added models and createUser service

---
+++ src/main/java/com/example/ShardedSagaWallet/services/UserService.java
@@ -0,0 +1,23 @@
+package com.example.ShardedSagaWallet.services;
+
+// Removed invalid import
+import org.springframework.stereotype.Service;
+
+import com.example.ShardedSagaWallet.entities.User;
+import com.example.ShardedSagaWallet.repository.UserRepository;
+
+import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserService {
+  private final UserRepository userRepository;
+ 
+   public User createUser(User user) {
+    log.info("Creating user: {}", user);
+    return userRepository.save(user);
+  }
+
+}
